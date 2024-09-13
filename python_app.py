6aa5c409326b51cb2bd8e5a5f2d2b7d55c4d6b1d
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-14 00:44:21 +0300

    Done! the program is done already

---
+++ python_app.py
@@ -0,0 +1,20 @@
+# collect the necessary inputs: principal, apr, years
+# calculate the monthly payment
+# show to the user
+
+def main():
+    print("This is a monthly payment loan Calculator ")
+    print("")
+    
+    principal = float(input("Input the loan amount: "))
+    apr = float(input("Input the annual interest nate: "))
+    years = float(input("Input the amount of years: "))
+    
+    monthly_interest_rate = apr / 1200
+    amount_of_months = years * 12
+    monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-amount_of_months))
+    
+    print("The monthly payment for this loan is : " + str(monthly_payment))
+    
+main()
+    
